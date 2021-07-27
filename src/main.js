2396d81ff44a7ffa6b4e6084675fa6d3cb00c935
Polpii <paulpeterarslan@gmail.com>
2021-07-27 15:33:53 +0200

    last

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import Vue from 'vue';
 import AspectRatio from 'v-aspect-ratio';
+import axios from 'axios';
+import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
 Vue.use(AspectRatio);
+Vue.prototype.$http = axios;
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false;
 
