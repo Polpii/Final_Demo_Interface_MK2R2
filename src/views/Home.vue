e483aff2de7240b5fe87023c5655e67fb801d9c6
Shkunn <leclercqted@gmail.com>
2021-08-09 20:54:57 +0200

    [CHANGE] Home <p>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app" v-aspect-ratio="'16:9'">
-    <p>{{this.$route.params }}</p>
     <img id="logo" src="../assets/DVIC_logo/dvic-large.png">
     <HeadBand :name="`${this.$route.params.id}`"/>
     <Hour/>
